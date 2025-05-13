673a78bd0f7f18fdc067ba2015707338dcfdf6ee
RaianRashidRimon <rimonraian2@gmail.com>
2025-05-14 00:54:18 +0600

    starter problem 6

---
+++ Starter Problems/Problem 6.py
@@ -0,0 +1,9 @@
+def landscape(width,height):
+    if(width>height):
+        print("The image is a landscape")
+    else:
+        print("This image is a portrait")
+
+landscape(280,300)
+landscape(1920,1080)
+landscape(720,1020)
